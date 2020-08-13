87a1fa1c4866450d9d4a2fdde34b49e95e264e0c
sergey_petrochenko <proffsneip@gmail.com>
2020-08-13 22:39:48 +0300

    Added search button

---
+++ movie-service/src/components/common/ButtonComponent.tsx
@@ -0,0 +1,19 @@
+import React, { MouseEvent, MouseEventHandler } from "react";
+
+export interface ButtonComponentProps {
+  className?: string;
+  onButtonClicked: MouseEventHandler;
+  text: string;
+}
+
+const ButtonComponent = (props: ButtonComponentProps) => {
+  return (
+    <>
+      <button className={props.className} onClick={props.onButtonClicked}>
+        {props.text}
+      </button>
+    </>
+  );
+};
+
+export default ButtonComponent;
--- movie-service/src/components/main/SearchComponent.tsx
+++ movie-service/src/components/main/SearchComponent.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import EditTextComponent from "../common/EditTextComponent";
 import TextComponent from "../common/TextComponent";
+import ButtonComponent from "../common/ButtonComponent";
 
 const SearchComponent = () => {
+  const onSearchButtonClicked = (event: React.MouseEvent) => {
+    console.log("button clicked");
+  };
+
   return (
     <div className="search">
       <TextComponent text="FIND YOUR MOVIE" className="findTextLabel" />
-      <EditTextComponent placeholder="What do you want to watch?" />
+      <div className="searchItems">
+        <EditTextComponent placeholder="What do you want to watch?" />
+        <ButtonComponent
+          className="searchButton"
+          text="SEARCH"
+          onButtonClicked={onSearchButtonClicked}
+        />
+      </div>
     </div>
   );
 };
